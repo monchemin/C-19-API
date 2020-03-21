f923b0a3c977d9664ed7f80e04e7582089cd5b63
nyemo <Geoleduc>
2020-03-21 00:44:07 -0400

    table creation

---
+++ database/health_contant.sql
@@ -0,0 +1,20 @@
+CREATE TABLE common.health_constant (
+	id uuid NOT NULL DEFAULT uuid_generate_v4(),
+	date_time timestamptz NOT NULL DEFAULT now(),
+	temperature float4 NOT NULL,
+	is_tired boolean NOT NULL,
+	has_dry_cough boolean NOT NULL,
+	has_shortness_of_breath boolean NOT NULL,
+	has_headache boolean NOT NULL,
+	has_ranny_nose boolean NOT NULL,
+	has_nasal_congestion boolean NOT NULL,
+	has_sore_throat boolean NOT NULL,
+	has_muscle_pain boolean NOT NULL,
+	patient_id uuid NOT NULL,
+	has_diarrhea boolean NOT NULL,
+	CONSTRAINT health_constant_pk PRIMARY KEY (id),
+	CONSTRAINT health_constant_fk FOREIGN KEY (patient_id) REFERENCES common.patient(patient_id)
+);
+CREATE INDEX health_constant_date_time_idx ON common.health_constant (date_time);
+CREATE INDEX health_constant_temperature_idx ON common.health_constant (temperature,is_tired,has_dry_cough,has_shortness_of_breath);
+CREATE INDEX health_constant_has_diarrhea_idx ON common.health_constant (has_diarrhea,has_muscle_pain,has_sore_throat,has_nasal_congestion,has_ranny_nose,has_headache);
---
+++ database/patient.sql
@@ -0,0 +1,22 @@
+//add this before create table statement
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE common.patient (
+	patient_id uuid NOT NULL DEFAULT uuid_generate_v4(),
+	phone_number varchar NOT NULL,
+	age int NULL,
+	is_diabetic boolean NULL DEFAULT false,
+	is_hypertensive boolean NULL DEFAULT false,
+	is_asthmastic boolean NULL DEFAULT false,
+	is_cardio_ischemic boolean NULL DEFAULT false,
+	has_lung_disease boolean NULL DEFAULT false,
+	has_kidney_disease boolean NULL DEFAULT false,
+	is_smoker boolean NULL DEFAULT false,
+	is_returns_from_travel boolean NULL DEFAULT false,
+	longitude double precision NOT NULL,
+	latitude double precision NOT NULL,
+	created_at timestamptz NOT NULL DEFAULT now(),
+	CONSTRAINT patient_pk PRIMARY KEY (patient_id),
+	CONSTRAINT patient_un UNIQUE (phone_number)
+);
+
